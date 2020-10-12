cdb781c3983e0c1d7277b59f6cb2a2fce425286c
Phattaraphon Pewruang <phattaraphon.pe.61@ubu.ac.th>
2020-10-13 05:10:55 +0700

    Updates

---
+++ precache-manifest.97def8caa0d89cd60c6c9e969604be57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5b3c431a148ada22a0c00abdba6541d",
+    "url": "/commath-project/index.html"
+  },
+  {
+    "revision": "910aacf5dac994e48089",
+    "url": "/commath-project/static/css/main.cbbcb3ef.chunk.css"
+  },
+  {
+    "revision": "70706fed4f63019ad6db",
+    "url": "/commath-project/static/js/2.ee47bf5b.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/commath-project/static/js/2.ee47bf5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "910aacf5dac994e48089",
+    "url": "/commath-project/static/js/main.615c8897.chunk.js"
+  },
+  {
+    "revision": "f91ed0674a7a29a50071",
+    "url": "/commath-project/static/js/runtime-main.b475e6e9.js"
+  }
+]);
