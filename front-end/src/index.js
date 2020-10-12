89b0f40523e5293e252224174e74ee9545365457
Phattaraphon Pewruang <phattaraphon.pe.61@ubu.ac.th>
2020-10-13 05:50:35 +0700

    deploy6

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,21 +3,49 @@ import ReactDOM from 'react-dom';
 import Page1 from './viws/Page1'
 import Page2 from './viws/Page2'
 import './index.css';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
 import Basic from './myform'
 class Game extends React.Component {
   render() {
     return (
-      <BrowserRouter>
+      
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/page1">
+          <Route path="/about">
             <Page1 />
           </Route>
-          <Route path="/page2">
+          <Route path="/users">
+            <Page2 />
+          </Route>
+          <Route path="/">
             <Page2 />
           </Route>
         </Switch>
-      </BrowserRouter>
+      </div>
+    </Router>
+
     );
   }
 }
--- front-end/src/viws/Page1.js
+++ front-end/src/viws/Page1.js
@@ -3,9 +3,8 @@ import React from 'react'
 export default function Page1() {
     return (
         <div>
-            <a href="/page2">
-            ssssssssss
-            </a>
+
+            sdfsdfsdf
         </div>
     )
 }
