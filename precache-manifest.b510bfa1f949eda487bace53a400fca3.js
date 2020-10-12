1557469a5655c65112ff9d72588464b7ea41f58b
Phattaraphon Pewruang <phattaraphon.pe.61@ubu.ac.th>
2020-10-13 05:21:52 +0700

    Updates

--- precache-manifest.b510bfa1f949eda487bace53a400fca3.js
+++ precache-manifest.b510bfa1f949eda487bace53a400fca3.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f27d0c19d651bad7e3651aaf79b76edd",
+    "revision": "602de4e2a54613da8e0b6b514c4dd113",
     "url": "/commath-project/index.html"
   },
   {
-    "revision": "8e706a81378521bf60fa",
+    "revision": "fb45402c78853d56616b",
     "url": "/commath-project/static/css/main.cbbcb3ef.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/commath-project/static/js/2.669e38b5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8e706a81378521bf60fa",
-    "url": "/commath-project/static/js/main.49ae8257.chunk.js"
+    "revision": "fb45402c78853d56616b",
+    "url": "/commath-project/static/js/main.1fb52044.chunk.js"
   },
   {
     "revision": "f91ed0674a7a29a50071",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/commath-project/precache-manifest.7e880b0cbb5064c84429964484ea21f9.js"
+  "/commath-project/precache-manifest.b510bfa1f949eda487bace53a400fca3.js"
 );
 
 self.addEventListener('message', (event) => {
