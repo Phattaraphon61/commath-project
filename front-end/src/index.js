235810dff79ecbdb04586bdd2f3abb880786c436
Phattaraphon Pewruang <phattaraphon.pe.61@ubu.ac.th>
2020-10-13 05:00:04 +0700

    createpage

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Page1 from './viws/Page1'
 import './index.css';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Basic from './myform'
 class Game extends React.Component {
   render() {
     return (
-      <div className="App">
-       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      </div>
+      <BrowserRouter>
+      <Switch>
+      <Route path="/home">
+            <Page1/>
+        </Route>
+        <Route path="/">
+        <Redirect to="/home" />
+      </Route>
+      </Switch>
+    </BrowserRouter>
     );
   }
 }
